fa10a95eca23534942b426515948a84ffb040eb6
anniemeng <anniezmeng@gmail.com>
2015-10-13 17:19:53 +0200

    implemented language checker n-grams from input

--- CrawlerProject/src/Dictionary/languageChecker.scala
+++ CrawlerProject/src/Dictionary/languageChecker.scala
@@ -1,7 +1,7 @@
 package Dictionary
 
 import scala.collection.mutable
-import scala.collection.mutable.{Map => MutMap}
+import scala.collection.mutable.{Map => MutMap,Set => MutSet}
 import java.io.File
 import scala.io.Source
 import scala.math.log
@@ -9,57 +9,86 @@ import scala.math.log
 object languageChecker {
   val nGramNumber = 3
   def main(args : Array[String]) = {
-    if (args.size != 2)
-      throw new Exception("Provide two files")
+    checkLanguage(List())
+  }
 
+  def checkLanguage(documents: List[String]) {
     // generate dictionary n-grams
-    val fileEN = args(0)
-    val fileDE = args(1)
+    val fileEN = "engDictionary"
+    val fileDE = "deDictionary"
 
     val enDict = ngrams(scala.io.Source.fromFile(fileEN).mkString, nGramNumber)
     val deDict = ngrams(scala.io.Source.fromFile(fileDE).mkString, nGramNumber)
-    //println(deDict.mkString("\n"))
 
-    //val normDict =
+    // normalize ngram count
+    val normDictEn = normalizeDict(enDict)
+    val normDictDe = normalizeDict(deDict)
 
-    val testStrings = List("a rose is a rose is a rose",
-      "a rose is a rose is a rose a","the documents","the documents","a rose")
+    if (documents.isEmpty) {
+      val testStrings = List("the Bachelor’s degree programmes begin in German",
+        "Das Bachelor-Studium beginnt auf Deutsch")
+    } else {
+      val testStrings = documents
+    }
 
     var numEnglish = 0
-    // split by new line?
     for (d <- testStrings) {
       val dGram = ngrams(d, nGramNumber)
-      val estimateEN = createEstimate(dGram, enDict)
-      val estimateDE = createEstimate(dGram, deDict)
-      if (isEnglish(dGram, estimateEN, estimateDE)) {
+      if (isEnglish(dGram.toMap, normDictEn, normDictDe)) {
         numEnglish += 1
+        //println("is english: " + d)
       }
     }
+
+    println("Unique English pages found: " + numEnglish)
   }
 
-  def ngrams(doc: String, n: Int): Map[String,Int] = {
-    if (n<1 || doc.length<n) Map()
+  def ngrams(doc: String, n: Int): MutMap[String,Int] = {
+    if (n < 1 || doc.length < n) MutMap()
     else {
       val result = MutMap[String,Int]()
       for (ngram <- doc.sliding(n))
-        result(ngram) = result.getOrElse(ngram,0)+1
-      result.toMap
+        result(ngram) = result.getOrElse(ngram, 0) + 1
+      result
     }
   }
 
-  /*def ngrams(doc: String, n: Int): Set[String] = {
-    if (n < 1 || doc.length < n) Set()
-    else {
-      val result = mutable.Set[String]()
-      for (ngram <- doc.sliding(n))
-        result += ngram
-      result.toSet
+  def normalizeDict(dict: MutMap[String, Int]): Map[String, Double] = {
+    val numGrams = dict.values.sum
+    dict.mapValues(count => (count.toDouble / numGrams) + 1).toMap
+  }
+
+  def isEnglish(grams:  Map[String, Int], enDict: Map[String, Double], deDict: Map[String, Double]): Boolean = {
+    var sumEN = 0.0
+    var sumDE = 0.0
+    for (s <- grams.keySet) {
+      //println("s string:" + s)
+      if (enDict.contains(s)) {
+        //println("s english val: " + log(enDict(s)))
+        sumEN += log(enDict(s) * grams(s))
+      }
+      if (deDict.contains(s)) {
+        //println("s german val: " + log(deDict(s)))
+        sumDE += log(deDict(s) * grams(s))
+      }
     }
-  }*/
 
-  def normalizeDict()
+    //println("sum english: " + sumEN)
+    //println("sum german: " + sumDE)
+    sumEN > sumDE
+  }
 
-  def createEstimate(doc: Set[String], dict: Set[String]) : Map[String, Double] = {
+  /*def ngrams(doc: String, n: Int): Set[String] = {
+  if (n < 1 || doc.length < n) Set()
+  else {
+    val result = mutable.Set[String]()
+    for (ngram <- doc.sliding(n))
+      result += ngram
+    result.toSet
+  }
+}*/
+
+  /*def createEstimate(doc: Set[String], dict: Set[String]) : Map[String, Double] = {
     val counts = mutable.Map[String, Int]()
     counts ++= dict.map(gram => (gram, 0))
 
@@ -71,14 +100,6 @@ object languageChecker {
     val numGrams = counts.values.sum
 
     counts.mapValues(count => (count.toDouble + 1) / (numGrams + dict.size)).toMap
-  }
+  }*/
 
-  def isEnglish(strings: Set[String], enDict: Map[String, Double], deDict: Map[String, Double]): Boolean = {
-    for (s <- strings) {
-      if (enDict.contains(s)) {
-
-      }
-    }
-    false
-  }
 }
